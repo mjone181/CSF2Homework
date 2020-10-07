8535f77f48628073d2faaaab857315e4f5e63892
Mack Jones <jonesmacka@outlook.com>
2020-10-07 11:07:25 -0500

    Added some functionality to Vehicle. Work in progress still.

---
+++ ClassesLibrary/Vehicle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    class Vehicle
+    {
+        //Create fields
+        private string _make;
+        private string _model;
+        private int _year;
+        private float _weight;
+
+        //Create properties
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }//end Make
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }//end Model
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }//end Year
+
+        public float Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }//end Weight
+
+        
+        //Create constructors
+        public Vehicle(string make, string model, int year, float weight)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Weight = weight;
+        }//end Vehicle
+
+        public Vehicle() { }//end Vehicle
+
+        //Create methods
+        public override string ToString()
+        {
+            return string.Format("");
+        }
+    }
+}
