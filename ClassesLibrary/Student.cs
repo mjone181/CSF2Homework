955a7393e5b84cddaca0acc77750165581101fd7
Mack Jones <jonesmacka@outlook.com>
2020-10-07 09:42:53 -0500

    Added Student.cs

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //Create fields
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //Create properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }//end FirstName
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }//end LastName
+
+        public string ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }//end ID
+
+        public float GPA
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+        //Create constructors
+        public Student (string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id;
+            GPA = gpa;
+        }//end Student
+
+        public Student() { }//end Student
+
+        //Create methods
+        public override string ToString()
+        {
+            return string.Format("First Name: {0}\n" +
+                "Last Name: {1}\n" +
+                "ID: {2}\n" +
+                "GPA: {3:n1}",
+                FirstName, LastName, ID, GPA);
+        }//end ToString()
+
+    }//end class
+}//end namespace
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ClassesLibrary;
 namespace TesterProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //making a new Student class for testing purposes and filling its variables.
+            Student s1 = new Student();
+            s1.FirstName = "John";
+            s1.LastName = "Brown";
+            s1.ID = "123456789";
+            s1.GPA = 4.0f;
+
+            //Display Student class to the screen.
+            Console.WriteLine(s1);
         }
     }
 }
